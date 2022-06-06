ecce53269c1f98ca898f56d96f01e96704b96116
Joyce <joycevandeng@gmail.com>
2022-06-06 15:12:18 +0200

    Opdrachten uitgevoerd

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,12 +1,15 @@
 
 
-// // changing individual properties with code and using setInterval
-// var rotationSpeed = 0.01;
-// var myOtherBox = document.getElementById('myOtherBox');
+// changing individual properties with code and using setInterval
+var rotationSpeed = -0.01;
+var myOtherBox = document.getElementById('myOtherBox');
 
-// function spin(){
-// 	myOtherBox.object3D.rotation.x += rotationSpeed;
-// 	console.log(myOtherBox.object3D.rotation.x);
-// }
+function spin(){
+	myOtherBox.object3D.rotation.x += rotationSpeed+2;
+	myOtherBox.object3D.rotation.y += rotationSpeed+2;
+	myOtherBox.object3D.rotation.z += rotationSpeed+2;
+	console.log(myOtherBox.object3D.rotation.x);
+	myOtherBox.object3D.scale.x += 1;
+}
 
-// setInterval(spin, 16); //equivalent to 60 fps
+setInterval(spin, 16); //equivalent to 60 fps
